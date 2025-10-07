e7624f1179f0aa4de4ba892207fe0d510c4f8ba8
Nico Brockmeyer <nb@datenschutz-scheerans.de>
2025-10-07 18:00:03 +0100

    added control layer to file_database system

---
+++ controller.py
@@ -0,0 +1,20 @@
+import crawler as cr
+import indexer as ind
+
+operation = input("Type 1, if you wanna download books or 2, if you wanna index all new books: ")
+match(operation):
+    case "1":
+        ids_string = input("Enter the IDs of all the book you wanna download (or a range like 1-10): ").strip().lower()
+        if '-' in ids_string:
+            id_first, id_last = map(int, ids_string.split('-'))
+            print("Downloading books in range", id_first, "to", id_last - 1)
+            cr.store_books(id_first, id_last)
+            print("Finished downloading books in range", id_first, "to", id_last - 1)
+        else:
+            ids = [int(id) for id in ids_string.replace(" ", "").split(",")]
+            for id in ids:
+                print("Downloading book with ID", id)
+                cr.store_books(id, id + 1)  # store_books expects a range, so we do id to id+1
+            print("Finished downloading books with IDs", ids)
+    case "2":
+        ind.reindex_all_books()
